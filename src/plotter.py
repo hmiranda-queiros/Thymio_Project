642304b489928486ba3e089493cce8afc31f7ad0
Hugo MIRANDA QUEIROS <hugomirandaqueiros@gmail.com>
2021-12-06 20:54:22 +0100

    correction finished

--- src/plotter.py
+++ src/plotter.py
@@ -2,61 +2,28 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Thymio outline
-center_offset = np.array([5.5,5.5])
-thymio_coords = np.array([[0,0], [11,0], [11,8.5], [10.2, 9.3], 
-                          [8, 10.4], [5.5,11], [3.1, 10.5], 
-                          [0.9, 9.4], [0, 8.5], [0,0]])-center_offset
-                          
-thymio_coords *= 10
-
-def rotate(angle, coords):
-    """
-    Rotates the coordinates of a matrix by the desired angle
-    :param angle: angle in radians by which we want to rotate
-    :return: numpy.array() that contains rotated coordinates
-    """
-    R = np.array(((np.cos(angle), -np.sin(angle)),
-                  (np.sin(angle),  np.cos(angle))))
     
-    return R.dot(coords.transpose()).transpose()
-     
-    
-def plot(ax1, ax2, ax3, ax4, x_est, P_est, fig):
-    rotated_thymio_coords = rotate(- x_est[2][0], thymio_coords) 
-    abs_Thymio_coords = rotated_thymio_coords + np.array([x_est[1][0], x_est[0][0]])
-    x1 = abs_Thymio_coords[:, 0]
-    y1 = abs_Thymio_coords[:, 1]
-    
-    x2, y2 = gauss(x_est[0][0], P_est[0][0])
-    x3, y3 = gauss(x_est[1][0], P_est[1][1])
-    x4, y4 = gauss(x_est[2][0], P_est[2][2], 1)
+def plot(ax1, ax2, ax3, x_est, P_est, fig):
+    x1, y1 = gauss(x_est[0][0], P_est[0][0])
+    x2, y2 = gauss(x_est[1][0], P_est[1][1])
+    x3, y3 = gauss(x_est[2][0], P_est[2][2], 1)
     
     ax1.clear()
     ax2.clear()
     ax3.clear()
-    ax4.clear()
-    
-    ax1.set_xlabel('Y')
-    ax1.set_ylabel('X')
-    ax1.set_xlim(0,1280)
-    ax1.set_ylim(0,720)
-    ax1.invert_yaxis()
-    ax1.set_aspect('equal', 'box')
 
-    ax2.set_xlabel('X')
+    ax1.set_xlabel('X (mm)')
+    ax1.set_ylabel('Gauss')
+    
+    ax2.set_xlabel('Y (mm)')
     ax2.set_ylabel('Gauss')
     
-    ax3.set_xlabel('X')
+    ax3.set_xlabel('Theta (deg)')
     ax3.set_ylabel('Gauss')
     
-    ax4.set_xlabel('Theta')
-    ax4.set_ylabel('Gauss')
-    
-    ax1.plot(x1, y1, 'b')
-    ax2.plot(x2, y2, 'g')
-    ax3.plot(x3, y3, 'r')
-    ax4.plot(x4, y4, 'y')
+    ax1.plot(x1, y1, 'g')
+    ax2.plot(x2, y2, 'r')
+    ax3.plot(x3, y3, 'y')
     fig.canvas.draw()
 
 def gauss(mu, var, deg = 0):
@@ -74,8 +41,6 @@ def gauss(mu, var, deg = 0):
     for e in x:
         y.append(1/math.sqrt(2 * math.pi * var) * math.exp(-(e - mu)**2/(2 * var)))
         
-        
-
     return x, y    
     
     
--- src/vision_utils.py
+++ src/vision_utils.py
@@ -81,6 +81,78 @@ def merge_img(map_img, thymio_img, y1, y2, x1, x2):
         map_img[y1:y2, x1:x2, c] = ((alpha_map*map_img[y1:y2,x1:x2,c]) + (alpha_thymio*thymio_img[yy1:yy2,xx1:xx2,c]))
     return map_img
 
+def localisation_cam_all(frame):
+    '''take camera instance as input, return if camera is ok, current Thymio pose, current obstacle map'''
+    red_lower = np.array([140, 90, 180])
+    red_upper = np.array([185, 160, 255])
+    # red_lower = np.array([140, 120, 150])
+    # red_upper = np.array([180, 180, 210])
+    # ret, frame = cap.read()
+    # while(not ret):
+    #     cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    #     cap.set(cv2.CAP_PROP_EXPOSURE, -4)
+    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    corners, ids, _ = aruco.detectMarkers(gray,aruco_dict,parameters=parameters)
+    corner_points = []  
+    #marker1=topleft   marker2=topright marker3=bottomleft marker4=bottomright
+    Thymio_start=[-1,-1]
+    Thymio_target=[-1,-1]
+    Thymio_dir = -1
+    obs_corners = []
+    obstacles_mask=[]
+    warpedimg_clean =[]
+    warpedimg=[]
+    img_out=np.ones((720,1280,3),np.uint8)*255
+    cam_OK = True
+    try:
+        for i in range(1,5):  
+            corner_points.append(corners[ids.tolist().index([i])][0][0].tolist())
+        pts1 = np.float32(corner_points)
+        pts2 = np.float32([[0, 0], [1280, 0], [0, 720], [1280, 720]])
+        transform = cv2.getPerspectiveTransform(pts1, pts2)
+        warpedimg = cv2.warpPerspective(frame, transform, (1280, 720))
+        corners, ids, _ = aruco.detectMarkers(warpedimg,aruco_dict,parameters=parameters)
+        Thymio_corner = corners[ids.tolist().index([0])]
+        rvec, tvec, _ = aruco.estimatePoseSingleMarkers(Thymio_corner, 0.05, camera_matrix, distortion)
+        Thymio_dir = -rotationMatrixToEulerAngles(rvec[0])[2]+180
+        Thymio_center = np.mean(Thymio_corner, axis=1).tolist()[0]
+        # Thymio_center = [int(Thymio_center[0]),int(Thymio_center[1])]
+        Thymio_center.reverse()
+        Thymio_target = np.mean(corners[ids.tolist().index([5])], axis=1).tolist()[0]
+        # Thymio_target = [int(Thymio_target[0]),int(Thymio_target[1])]
+        Thymio_target.reverse()
+        HSV = cv2.cvtColor(warpedimg, cv2.COLOR_RGB2HSV)
+        HSV_blur = cv2.GaussianBlur(HSV, (7, 7), 0)
+        red_mask=cv2.inRange(HSV_blur,red_lower,red_upper)
+        red_mask_closed = cv2.morphologyEx(red_mask, cv2.MORPH_CLOSE, np.ones((5,5), np.uint8),iterations=2)
+        obstacles_mask = cv2.dilate(red_mask_closed, np.ones((int(ext_pixels*2-1),int(ext_pixels*2-1)), np.uint8), iterations=1)
+        
+        contours, hierarchy = cv2.findContours(obstacles_mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        obstacles_mask[obstacles_mask==255]=1
+        for cnt in contours:
+            if cv2.contourArea(cnt) > 1000:#drop spots
+                temp = []
+                epsilon = TH_Poly*cv2.arcLength(cnt,True)
+                approx = cv2.approxPolyDP(cnt,epsilon,True)
+                for point in approx:
+                    point_original = list(point[0])
+                    temp.append([point_original[1],point_original[0]])
+                obs_corners.append(temp)
+
+
+        
+        _, mask = cv2.threshold(red_mask, thresh=180, maxval=255, type=cv2.THRESH_BINARY)
+        img_out[mask==255] = [200,20,20]
+        Thymio_start = Thymio_center
+        # thymio_icon = cv2.imread('Thymio.jpg')
+        # thymio_icon = cv2.cvtColor(thymio_icon, cv2.COLOR_RGB2BGR)
+        # warpedimg_clean = merge_img(img_out, rotate_image(thymio_icon,Thymio_dir), int(Thymio_center[0])-120, int(Thymio_center[0])+120, int(Thymio_center[1])-120, int(Thymio_center[1])+120)
+        # warpedimg_clean = warpedimg_clean[:,:,0:3]
+    except:
+        cam_OK=False
+    return cam_OK, Thymio_start, Thymio_dir, Thymio_target, obs_corners, obstacles_mask, img_out
+
 def localisation_cam(frame):
     '''take camera instance as input, return if camera is ok, current Thymio pose, current obstacle map'''
     red_lower = np.array([140, 90, 200])
@@ -116,40 +188,11 @@ def localisation_cam(frame):
         rvec, tvec, _ = aruco.estimatePoseSingleMarkers(Thymio_corner, 0.05, camera_matrix, distortion)
         Thymio_dir = -rotationMatrixToEulerAngles(rvec[0])[2]+180
         Thymio_center = np.mean(Thymio_corner, axis=1).tolist()[0]
-        Thymio_center = [int(Thymio_center[0]),int(Thymio_center[1])]
+        # Thymio_center = [int(Thymio_center[0]),int(Thymio_center[1])]
         Thymio_center.reverse()
-        Thymio_target = np.mean(corners[ids.tolist().index([5])], axis=1).tolist()[0]
-        Thymio_target = [int(Thymio_target[0]),int(Thymio_target[1])]
-        Thymio_target.reverse()
-        HSV = cv2.cvtColor(warpedimg, cv2.COLOR_RGB2HSV)
-        HSV_blur = cv2.GaussianBlur(HSV, (7, 7), 0)
-        red_mask=cv2.inRange(HSV_blur,red_lower,red_upper)
-        red_mask_closed = cv2.morphologyEx(red_mask, cv2.MORPH_CLOSE, np.ones((5,5), np.uint8),iterations=2)
-        obstacles_mask = cv2.dilate(red_mask_closed, np.ones((int(ext_pixels*2-1),int(ext_pixels*2-1)), np.uint8), iterations=1)
-        
-        contours, hierarchy = cv2.findContours(obstacles_mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        obstacles_mask[obstacles_mask==255]=1
-        for cnt in contours:
-            if cv2.contourArea(cnt) > 1000:#drop spots
-                temp = []
-                epsilon = TH_Poly*cv2.arcLength(cnt,True)
-                approx = cv2.approxPolyDP(cnt,epsilon,True)
-                for point in approx:
-                    point_original = list(point[0])
-                    temp.append([point_original[1],point_original[0]])
-                obs_corners.append(temp)
-
-
-        img_out=np.ones((HSV_blur.shape[0],HSV_blur.shape[1],3),np.uint8)*255
-        _, mask = cv2.threshold(red_mask, thresh=180, maxval=255, type=cv2.THRESH_BINARY)
-        img_out[mask==255] = [200,20,20]
-        thymio_icon = cv2.imread('Thymio.jpg')
-        thymio_icon = cv2.cvtColor(thymio_icon, cv2.COLOR_RGB2BGR)
-        warpedimg_clean = merge_img(img_out, rotate_image(thymio_icon,Thymio_dir), int(Thymio_center[0])-120, int(Thymio_center[0])+120, int(Thymio_center[1])-120, int(Thymio_center[1])+120)
-        warpedimg_clean = warpedimg_clean[:,:,0:3]
     except:
         cam_OK=False
-    return cam_OK, Thymio_center, Thymio_dir, Thymio_target, obs_corners, obstacles_mask, warpedimg, warpedimg_clean
+    return cam_OK, Thymio_center, Thymio_dir, warpedimg
 
 def swap(obs):
     obs_coords_swap = []
@@ -165,20 +208,21 @@ def obs_corners_point(obs_corners_swap):
     point_list = [tuple(item) for sublist in tmp for item in sublist]
     return point_list
 
-def overlay_vis_graph(Thymio_center, Thymio_target, obs_corners, optimal_path, warpedimg_clean):
+def overlay_vis_graph(Thymio_start, Thymio_center, Thymio_target, obs_corners, optimal_path, warpedimg_clean):
     obs_corners_swap = swap(obs_corners)
     point_list = obs_corners_point(obs_corners_swap)
     warpedimg_clean_cpy = warpedimg_clean.copy()
     cv2.polylines(warpedimg_clean_cpy, obs_corners_swap, isClosed=True, color=(0, 114, 189), thickness=8)
     for point in point_list:
         cv2.circle(warpedimg_clean_cpy, point, 5, color=(217, 83, 25), thickness=10)
+    cv2.circle(warpedimg_clean_cpy, tuple([Thymio_start[1],Thymio_start[0]]), 5, color=(126, 47, 142), thickness=12)
     cv2.circle(warpedimg_clean_cpy, tuple([Thymio_center[1],Thymio_center[0]]), 5, color=(126, 47, 142), thickness=12)
     cv2.circle(warpedimg_clean_cpy, tuple([Thymio_target[1],Thymio_target[0]]), 5, color=(126, 47, 142), thickness=12)
     for i in range(len(optimal_path)-1):
-        cv2.line(warpedimg_clean_cpy,tuple([optimal_path[i][1][1],optimal_path[i][1][0]]),
-                tuple([optimal_path[i+1][1][1],optimal_path[i+1][1][0]]),
+        cv2.line(warpedimg_clean_cpy,tuple([int(optimal_path[i][1][1]),int(optimal_path[i][1][0])]),
+                tuple([int(optimal_path[i+1][1][1]),int(optimal_path[i+1][1][0])]),
                 color=(119, 172, 48),
                 thickness=12)
-    cv2.putText(warpedimg_clean_cpy, 'Start', tuple([Thymio_center[1],Thymio_center[0]]), fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.5, color = (0,0,0),thickness = 7)
+    cv2.putText(warpedimg_clean_cpy, 'Start', tuple([Thymio_start[1],Thymio_start[0]]), fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.5, color = (0,0,0),thickness = 7)
     cv2.putText(warpedimg_clean_cpy, 'Goal', tuple([Thymio_target[1],Thymio_target[0]]), fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.5, color = (0,0,0),thickness = 7)
     return(warpedimg_clean_cpy)
